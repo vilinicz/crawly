ee095b2154529f3b4d09bd9e64ab376865e9eb7d
vilinicz <v.ilinicz@gmail.com>
2024-12-03 23:37:32 +0300

    add basic google engine

--- search/config.py
+++ search/config.py
@@ -1,15 +1,19 @@
 import httpx
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1636.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate",
     "DNT": "1",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
+    "authority": "www.google.com",
+    "Cookie": "AEC=AZ6Zc-XhLv-TOcUV5nktjBA3lb8uUM-Isq3Nv0QqMOPWRW7-ILg5OyiJzA; SOCS=CAISHAgCEhJnd3NfMjAyNDExMjYtMF9SQzEaAm5sIAEaBgiAlLm6Bg; NID=519=F_pSyOUcs2wVQ-5aU8ggotrGEtcstvf9oLEw0F7Yf5BOjP9tX9ZrYqBmVjjlBkVJLq_wJePqnlgP8dlvlY6lcGugTZBWU_UN3PYT8hDTO0VX35H-7cFwhqSeMiM9Kf7AlXPMGQHjy2PWtYyTSKQGETTLqipc_V5JzxTQByoLq2Yjupy9hmtpNVytaX1cZfvOPhn5YI_HhY3ZJK13xtsdaVIYTgQ9TtIb9mw4Yvxim4YYaiqeQgKa; DV=Q7rDc4E0MeITMLoqspk1c5HHZ53TOBk",
 }
 
 
 def default_http_client():
-    return httpx.AsyncClient(timeout=5, headers=headers, follow_redirects=True)
+    return httpx.AsyncClient(
+        timeout=5, headers=headers, follow_redirects=True, http2=True
+    )
--- search/engines/google.py
+++ search/engines/google.py
@@ -1,5 +1,7 @@
 import logging
 
+from bs4 import BeautifulSoup
+
 from entry import EntryCollection
 from search.engine import Engine
 
@@ -7,18 +9,29 @@ from search.engine import Engine
 class Google(Engine):
     def __init__(self, http_client):
         super().__init__(http_client)
-        self.url = "https://google.com"
+        self.url = "https://www.google.com/search"
         self.logger = logging.getLogger("google")
 
     async def search(self, q: str) -> EntryCollection:
         self.logger.info("Searching for %s", q)
-        return EntryCollection(
-            [
-                {
-                    "engine": self.__class__.__name__.lower(),
-                    "title": f"{q} result {i}",
-                    "url": f"https://google.com/{i}",
-                }
-                for i in range(1)
-            ]
-        )
+        async with self.http_client() as client:
+            response = await client.get(self.url, params={"q": q})
+        return EntryCollection(self._extract_links(response))
+
+    def _extract_links(self, response):
+        soup = BeautifulSoup(response.text, "html.parser")
+        div_tags = soup.body.find_all("div", class_="g")
+        link_tags = [div.find("a") for div in div_tags]
+        links: list[dict] = []
+        for link in link_tags:
+            title = link.find("h3").text
+            url = link.get("href")
+            if not any(item["url"] == url and item["title"] == title for item in links):
+                links.append(
+                    {
+                        "engine": self.__class__.__name__.lower(),
+                        "title": link.find("h3").text,
+                        "url": link.get("href"),
+                    }
+                )
+        return links
